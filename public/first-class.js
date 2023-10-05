3762af06fbf0ac8961edd2b79246b5daa467be82
John Lundgren <john.paul.lundgren@gmail.com>
2023-10-05 11:09:23 +0200

    Same functionality with OOP.

--- public/first-class.js
+++ public/first-class.js
@@ -1,3 +1,7 @@
+/*
+Functional Programming 
+*/
+
 const greet = (greeting) => {
   return (name) => {
     console.log(`${greeting} ${name}`);
@@ -13,3 +17,22 @@ const hola = greet('Hola');
 
 hola('José');
 hola('Pedro');
+
+/*
+Object Oriented Programming
+*/
+
+class Greeter {
+  constructor(greeting) {
+    this.greeting = greeting;
+  }
+  greet(name) {
+    console.log(`${this.greeting} ${name}`);
+  }
+}
+
+const ciao = new Greeter('Ciao');
+ciao.greet('Araceli');
+
+const howdy = new Greeter('Howdy');
+howdy.greet('Hanna');
